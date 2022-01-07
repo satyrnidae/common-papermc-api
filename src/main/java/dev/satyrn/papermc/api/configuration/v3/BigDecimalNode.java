cf7c59d02abfc7f31fc7a0e7d54717d385ee9d74
Isabel Maskrey <isabel@satyrn.dev>
2022-01-07 06:27:09 -0500

    ✨ Create BigDecimalNode.java
    Add a new node for big decimal values.

---
+++ src/main/java/dev/satyrn/papermc/api/configuration/v3/BigDecimalNode.java
@@ -0,0 +1,56 @@
+package dev.satyrn.papermc.api.configuration.v3;
+
+import dev.satyrn.papermc.api.configuration.v1.ConfigurationContainer;
+import dev.satyrn.papermc.api.configuration.v1.ConfigurationNode;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+
+/**
+ * Represents a configurable BigDecimal value.
+ *
+ * @author Isabel Maskrey
+ * @since 1.3-SNAPSHOT
+ */
+public class BigDecimalNode extends ConfigurationNode<BigDecimal> {
+    /**
+     * Initializes a new Configuration node.
+     *
+     * @param parent The parent node.
+     * @param name The node's name.
+     * @since 1.3-SNAPSHOT
+     */
+    public BigDecimalNode(@NotNull ConfigurationContainer parent, @NotNull String name) {
+        super(parent, name, parent.getConfig());
+    }
+
+    /**
+     * Gets the value of the node.
+     *
+     * @return The value.
+     * @since 1.3-SNAPSHOT
+     */
+    @Override
+    public final @NotNull BigDecimal value() {
+        final String stringValue = this.getConfig().getString(this.getPath());
+        if (stringValue == null) {
+            return this.defaultValue();
+        }
+        try {
+            return new BigDecimal(stringValue);
+        } catch (NumberFormatException ex) {
+            return this.defaultValue();
+        }
+    }
+
+    /**
+     * Gets the default value of the node.
+     *
+     * @return The value.
+     * @since 1.3-SNAPSHOT
+     */
+    @Override
+    public @NotNull BigDecimal defaultValue() {
+        return BigDecimal.ZERO;
+    }
+}
