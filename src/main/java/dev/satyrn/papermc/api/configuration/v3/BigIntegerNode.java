fd6800722c9ca980afe25df632ef1ca985394084
Isabel Maskrey <isabel@satyrn.dev>
2022-01-07 06:27:37 -0500

    ✨ Create BigIntegerNode.java
    Create a new node for big integer values.

---
+++ src/main/java/dev/satyrn/papermc/api/configuration/v3/BigIntegerNode.java
@@ -0,0 +1,57 @@
+package dev.satyrn.papermc.api.configuration.v3;
+
+import dev.satyrn.papermc.api.configuration.v1.ConfigurationContainer;
+import dev.satyrn.papermc.api.configuration.v1.ConfigurationNode;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
+
+/**
+ * Represents a configurable BigInteger value.
+ *
+ * @author Isabel Maskrey
+ * @since 1.3-SNAPSHOT
+ */
+public class BigIntegerNode extends ConfigurationNode<BigInteger> {
+
+    /**
+     * Initializes a new Configuration node.
+     *
+     * @param parent The parent node.
+     * @param name The node name.
+     * @since 1.0-SNAPSHOT
+     */
+    protected BigIntegerNode(@NotNull ConfigurationContainer parent, @NotNull String name) {
+        super(parent, name, parent.getConfig());
+    }
+
+    /**
+     * Gets the value of the node.
+     *
+     * @return The value.
+     * @since 1.0-SNAPSHOT
+     */
+    @Override
+    public final @NotNull BigInteger value() {
+        final String stringValue = this.getConfig().getString(this.getPath());
+        if (stringValue == null) {
+            return this.defaultValue();
+        }
+        try {
+            return new BigInteger(stringValue);
+        } catch (NumberFormatException ex) {
+            return this.defaultValue();
+        }
+    }
+
+    /**
+     * Gets the default value of the node.
+     *
+     * @return The value.
+     * @since 1.3-SNAPSHOT
+     */
+    @Override
+    public @NotNull BigInteger defaultValue() {
+        return BigInteger.ZERO;
+    }
+}
