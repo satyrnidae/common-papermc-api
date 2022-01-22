694abb45258ede13e0133eda91cda3016c4365b0
Isabel Maskrey <isabel@satyrn.dev>
2022-01-22 13:47:52 -0500

    🗃️ Create a MySQL-Like data store connection manager
    Created a new connection manager to connect to a mysql-like data source

---
+++ src/main/java/dev/satyrn/papermc/api/storage/v1/ConnectionManager.java
@@ -0,0 +1,21 @@
+package dev.satyrn.papermc.api.storage.v1;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.Connection;
+
+/**
+ * Manages connections to a data source.
+ *
+ * @author Isabel Maskrey
+ * @since 1.6.0
+ */
+public interface ConnectionManager {
+    /**
+     * Opens a connection to the data source.
+     *
+     * @return The connection to the data source.
+     * @since 1.6.0
+     */
+    @Nullable Connection connect();
+}
---
+++ src/main/java/dev/satyrn/papermc/api/storage/v1/MySQLConnectionManager.java
@@ -0,0 +1,70 @@
+package dev.satyrn.papermc.api.storage.v1;
+
+import dev.satyrn.papermc.api.configuration.v5.MySQLConfiguration;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+
+/**
+ * Handles connections to a MySQL-like data source.
+ *
+ * @author Isabel Maskrey
+ * @since 1.6.0
+ */
+@SuppressWarnings({"unused", "ClassCanBeRecord"})
+public class MySQLConnectionManager implements ConnectionManager {
+    private final transient @NotNull Plugin plugin;
+    private final transient @NotNull MySQLConfiguration configuration;
+
+    public MySQLConnectionManager(final @NotNull Plugin plugin, final @NotNull MySQLConfiguration configuration) {
+        this.plugin = plugin;
+        this.configuration = configuration;
+    }
+
+    /**
+     * Opens a connection to the data source.
+     *
+     * @return The connection to the data source.
+     * @since 1.6.0
+     */
+    @Override
+    public @Nullable Connection connect() {
+        final @NotNull StringBuilder connectionURLBuilder = new StringBuilder("jdbc:mysql://").append(this.configuration.hostname.value())
+                .append(':')
+                .append(this.configuration.port.value())
+                .append('/')
+                .append(this.configuration.database.value());
+
+        final @NotNull List<Map<?, ?>> flagsList = this.configuration.flags.value();
+        if (!flagsList.isEmpty()) {
+            int i = 0;
+            for (final @NotNull Map<?, ?> flags : flagsList) {
+                for (final @NotNull Map.Entry<?, ?> entry : flags.entrySet()) {
+                    connectionURLBuilder.append(i++ == 0 ? '?' : '&')
+                            .append(entry.getKey().toString())
+                            .append('=')
+                            .append(entry.getValue().toString());
+                }
+            }
+        }
+
+        @Nullable Connection connection = null;
+        try {
+            final @NotNull String connectionURL = connectionURLBuilder.toString();
+            final @Nullable String userID = this.configuration.userID.value();
+            this.plugin.getLogger().log(Level.FINE, String.format("[Storage] Attempting connection to MySQL-like database at %s with %s", connectionURL, userID == null ? "anonymous user" : "user " + userID));
+            connection = DriverManager.getConnection(connectionURL, userID, this.configuration.password.value());
+        } catch (SQLException ex) {
+            this.plugin.getLogger().log(Level.SEVERE, String.format("[Storage] Failed to connect to the database: %s", ex.getMessage()), ex);
+        }
+
+        return connection;
+    }
+}
